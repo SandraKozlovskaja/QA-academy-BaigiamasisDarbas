80a93efaeb901297f6304c74a2c671ed8941f16f
SandraKozlovskaja <kosandra97@gmail.com>
2021-09-07 21:05:41 +0300

    Add files via upload

---
+++ BaigiamasisTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace autotests.Test
+{
+    public class BaigiamasisTest : BaseTest
+    {
+        [Test]
+        public static void Category()
+        {
+            public void NavigateToPage();
+            public void maistoParuosimasClick();
+            public void darzoviuPjausktyklesClick();
+        }
+
+        
+    }
+}
